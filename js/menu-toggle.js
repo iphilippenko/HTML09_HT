1eb080ead41e16d8eea5b5ca64a2b509b807e9fb
Irina Pylypenko <pylypenko.ira.vad@gmail.com>
2016-12-22 12:15:16 +0200

    mobile

--- js/menu-toggle.js
+++ js/menu-toggle.js
@@ -1,12 +1,10 @@
 document.getElementById('menu-toggle-btn').addEventListener("click", function() {
     document.getElementById('menu-btn-container').classList.toggle('active');
     document.getElementById('menu').classList.toggle('active');
-    document.getElementById('menu-btn-container').style.display="none";
 });
 
 document.getElementById('menu-close-btn').addEventListener("click", function() {
     document.getElementById('menu').classList.toggle('active');
     document.getElementById('menu-close-btn').classList.toggle('active');
     document.getElementById('menu-btn-container').classList.toggle('active');
-    document.getElementById('menu-btn-container').style.display="block";
 });
